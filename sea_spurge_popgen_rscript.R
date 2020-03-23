856e4b88d9184f5a2ea2991c83a9ceb86bf72b85
gchunter <61396043+gchunter@users.noreply.github.com>
2020-03-23 20:02:00 +1100

    All filtering

--- sea_spurge_popgen_rscript.R
+++ sea_spurge_popgen_rscript.R
@@ -1,3 +1,4 @@
+#Load dartR
 library(dartR)
 #Assign dartseq file and ind file to a genlight object
 gl_all_samples <- gl.read.dart(filename = "data/Report_DEup19-4268_1_moreOrders_SNP_1.csv",
@@ -23,6 +24,25 @@ gl.report.callrate(gl_all_samples)
 gl_filter1_callrate <- gl.filter.callrate(gl_all_samples, method = "loc",
                                           threshold = 0.90, recalc = TRUE, plot = TRUE)
 #filter on departure of Hardy-Weinberg equilibrium for every loci
-gl.filter2_hwe <- gl.filter.hwe(gl_filter1_callrate,
+gl_filter2_hwe <- gl.filter.hwe(gl_filter1_callrate,
                                 alpha = 0.05, basis = "any",
                                 bon = TRUE, v = 5)
+#report the minor allele frequencies for filter2_HWE
+gl.report.maf(gl.filter2_hwe, v = 5)
+#filter on MAF at 0.01
+gl_filter3_maf <- gl.filter.maf(gl_filter2_hwe, threshold = 0.01, v = 5)
+#report the reproducibility for the loci
+gl.report.repavg(gl_filter3_maf)
+#filter out loci for thich the repeateability is less than 0.98
+gl_filter4_repavg <- gl.filter.repavg(gl_filter3_maf, threshold = 0.98, v = 3)
+#report the number of monomorphs
+gl.report.monomorphs(gl_filter4_repavg)
+#report secondaries
+gl.report.secondaries(gl_filter4_repavg)
+#filter out secondaries
+gl_filter5_secondaries <- gl.filter.secondaries(gl_filter4_repavg, method = "random", v = 5)
+#produce a smearplot
+glPlot(gl_filter5_secondaries)
+
+
+
