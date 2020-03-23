21a716d509e7341b085055944f0fff9630fafcab
gchunter <61396043+gchunter@users.noreply.github.com>
2020-03-23 17:49:45 +1100

    first filter steps

--- sea_spurge_popgen_rscript.R
+++ sea_spurge_popgen_rscript.R
@@ -1,1 +1,28 @@
-#start project
+library(dartR)
+#Assign dartseq file and ind file to a genlight object
+gl_all_samples <- gl.read.dart(filename = "data/Report_DEup19-4268_1_moreOrders_SNP_1.csv",
+                   ind.metafile = "data/sea_spurge_ind2_metrics.csv")
+#structure of the genlight object
+gl_all_samples
+#number of individuals in the genlight object
+nInd(gl_all_samples)
+#number of loci
+nLoc(gl_all_samples)
+#number of populations
+nPop(gl_all_samples)
+#populaitons to which the individuals belong
+pop(gl_all_samples)
+#table of individuals per population
+table(pop(gl_all_samples))
+#population names
+levels(pop(gl_all_samples))
+#calculate and report the overall call rate across the loci
+#Use a missing threshold value < 10% (2202 loci retained)
+gl.report.callrate(gl_all_samples)
+#filter on call rate and retain only those loci with less than 10% missing data
+gl_filter1_callrate <- gl.filter.callrate(gl_all_samples, method = "loc",
+                                          threshold = 0.90, recalc = TRUE, plot = TRUE)
+#filter on departure of Hardy-Weinberg equilibrium for every loci
+gl.filter2_hwe <- gl.filter.hwe(gl_filter1_callrate,
+                                alpha = 0.05, basis = "any",
+                                bon = TRUE, v = 5)
