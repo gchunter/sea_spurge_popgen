a19e8a85be63d5666c7bcd166edbd62c53d8d98c
gchunter <61396043+gchunter@users.noreply.github.com>
2020-04-22 21:12:48 +1000

    included hierfsta and strataG functions

--- sea_spurge_popgen_rscript.R
+++ sea_spurge_popgen_rscript.R
@@ -1,5 +1,10 @@
-#Load dartR
+#Load packages
 library(dartR)
+library(hierfstat)
+library(adegenet)
+library(tidyverse)
+library(ggplot2)
+library(strataG)
 #Assign dartseq file and ind file to a genlight object
 gl_all_samples <- gl.read.dart(filename = "data/Report_DEup19-4268_1_moreOrders_SNP_1.csv",
                    ind.metafile = "data/sea_spurge_ind2_metrics.csv")
@@ -52,68 +57,107 @@ gl_filter6_ind_cr <- gl.filter.callrate(gl_filter5_secondaries,
 gl_filter7_loc_cr <- gl.filter.callrate(gl_filter6_ind_cr,
                                         method = "loc", threshold = 0.95, recalc = TRUE, 
                                         mono.rm = TRUE, plot = TRUE, v = 2)
-
 #population names in gl_filter7_loc_cr
 popNames(gl_filter7_loc_cr)
 #convert genlight to treemix input file format
 gl2treemix(gl_filter7_loc_cr, outpath = "data")
 #change from genlight to genind
 gind_filter7_loc_cr <- gl2gi(gl_filter7_loc_cr, v =1)
-#Load Hierfstat
-library(hierfstat)
+#convert genind to gtypes
+gi_f7_g <- genind2gtypes(gind_filter7_loc_cr)
+#calculate all summary stats for each stratum(population) seperately
+allsum_gi_f7_g <- summarizeLoci(gi_f7_g, by.strata = TRUE)
+write.csv(allsum_gi_f7_g,
+          file = "results/allsum_gi_f7_g.csv",
+          row.names = TRUE, col.names = TRUE)
+#Calculate the number of private alleles in each strata and locus
+pa_gi_f7_g <- privateAlleles(gi_f7_g)
+#write a csv of the private alleles object
+write.csv(pa_gi_f7_g, file = "results/pa_gi_f7_g.csv",
+          row.names = TRUE, col.names = TRUE)
 #convert genind to hierfstat format
 hf_filter7 <- genind2hierfstat(gind_filter7_loc_cr, pop = NULL)
 attach(hf_filter7)
 hf_filter7_loci <- hf_filter7[,c(2:752)]
 hf_filter7_pop <- hf_filter7$pop
-
 hf_filter7_basic_stats <- basic.stats(data.frame(hf_filter7_pop, 
                                                  hf_filter7_loci),
             diploid = TRUE, digits = 4)
-#calculate allelic richness
-allelic.richness(hf_filter7)
+#Calculate basic.stats for hf_filter7 and assign to object
+hf_f7_bs <- basic.stats(hf_filter7, diploid = TRUE, digits = 4)
+#allelic richness
+hf_f7_ar <- allelic.richness(hf_filter7, min.n = NULL, diploid = TRUE)
+#View allelic richness results
+View(hf_f7_ar[["Ar"]])
+#make a table of Ar values from hf_filter7
+table_hf_f7_ar <- hf_f7_ar[["Ar"]]
+#write a .csv file of table_hf_f7_ar
+write.csv(table_hf_f7_ar,
+              file = "results/table_hf_f7_ar.csv",
+              append = FALSE, quote = FALSE, row.names = TRUE,
+              col.names = TRUE)
+
+
+#Make a table of Hs values only from hf_f7_bs
+table_hf_f7_bs_Hs <- hf_f7_bs[["Hs"]]
+#write a .csv file of table_hf_f7_bs_Hs
+write.csv(table_hf_f7_bs_Hs,
+          file = "results/table_hf_f7_bs_Hs.csv",
+          append = FALSE, quote = FALSE, row.names = TRUE,
+          col.names = TRUE)
+#create a dataframe of Hs from the .csv file
+df_hf_f7_bs_Hs <- read_csv("results/table_hf_f7_bs_Hs.csv")
+#remove column X1
+df2_hf_f7_bs_Hs <- select(df_hf_f7_bs_Hs, -X1)
+#View Fis
+view(hf_f7_bs[["Fis"]])
+#make a table of Fis values only from hf_f7_bs
+table_hf_f7_bs_Fis <- hf_f7_bs[["Fis"]]
+#write a .csv of the table_hf_f7_bs_Fis
+write.csv(table_hf_f7_bs_Fis, file = "results/table_hf_f7_bs_Fis.csv",
+          append = FALSE, quote = FALSE, row.names = TRUE,
+          col.names = TRUE)
+#View Ho
+view(hf_f7_bs[["Ho"]])
+#Make a table of Ho values only from hf_f7_bs
+table_hf_f7_bs_Ho <- hf_f7_bs[["Ho"]]
+#Write a .csv of the object table_hf_f7_bs_Ho
+write.csv(table_hf_f7_bs_Ho,
+          file = "results/table_hf_f7_bs_Ho.csv",
+          append = FALSE, quote = FALSE, row.names = TRUE,
+          col.names = TRUE)
+#calculate allelic richness and print output to csv
+hf_filter7_ar <- allelic.richness(data.frame(hf_filter7_pop,
+                                             hf_filter7_loci),
+                                  min.n = NULL,
+                                  diploid = TRUE)
+table_ar <- hf_filter7_ar[["Ar"]]
+write.csv(table_ar, file = "hierfstat/table_ar.csv", row.names = TRUE,
+          col.names = TRUE)
+table(hf_filter7[,1])
 #calculate basic statistics
 hf_filter7_basic_stats <- basic.stats(hf_filter7)
+#TREEMIX
+#plot tree of m3 bootstrap 100 for gl_filter7_loc_cr
+source("treemix/plotting_funcs.R")
+plot_tree("treemix/m2/gl7m2b100_stem")
+plot_resid("treemix/m4/gl7m4b100_stem", "treemix/poporder2")
 
-hf_filter7
+poporder <- list("Anxious Bay beach Elliston", "Aslings Beach Eden", "Balnarring Beach",
+                 "Bawley Point", "Beach near Lake Bunga", "Bennetts Beach Yacaaba Peninsula",
+                 "Bernie Central", "Birthday Bay Southwest", "Blossom Beach", "Bmerwerre Beach",                   
+                 "Boydtown beach", "Broulee South", "Brown Bay Port MacDonnell",
+                 "Camel Rock Beach", "Cape Borks Lighthouse", "Dunes Beach south west",            
+                 "Endeovon Bay SouthWest", "Esperence Harbour Beach", 
+                 "Greens Beach", "Hankerchief beach","Hardwick Bay", "Hilkier Bay", 
+                 "Kilcunda", "Lagoon Beach Lord Howe Island", "Loaders", 
+                 "Lone Pine Sleaford", "Lowlands Beach", "Lucky Bay", 
+                 "Maria Island", "Myponga beach", "North Bernie", "Pambula Beach", 
+                 "Pardoe Northdown", "Pardoe Northdown Conservation Area", 
+                 "Peppermint Grove Beach", "Port Latta", "Port Neill Foreshore Beach", 
+                 "Princetown Beach", "Pt Lesueur Maria Island", "Riedle Beach",
+                 "Rifle Butts Beach Port Victoria", "Silverleaves Phillip Island",
+                 "Ski Beach Tumby Bay", "Squeaky Beach", "Stansbury", "Steamers Beach", 
+                 "Tea Tree Lane", "Venus Bay", "West Beach Hopetoun", 
+                 "Woolamai Beach Phillip Island", "Yanchep")
 
-?genind2hierfstat
-
-write.csv(hf_filter7, file = "hf_filter7.csv")
-
-
-
-
-
-head(hf_filter7)
-table(diploid[,1])
-table(hf_filter7[,1])
-data(gind_filter7_loc_cr)
-head(genind2hierfstat(gind_filter7_loc_cr)[,1:10])
-dim(hf_filter7)
-gind_filter7_loc_cr
-allele.count(hf_filter7, diploid = TRUE)
-allelic.richness(hf_filter7, diploid = TRUE)
-hf_ar <- allelic.richness(hf_filter7, diploid = TRUE)
-rm(hf_ar)
-hf_boot_fis <- boot.ppfis(hf_filter7,
-                          nboot = 10,
-                          quant = c(0.025, 0.975),
-                          diploid = TRUE,
-                          dig = 3)
-hf_boot_fis
-ho <- hf_filter7_basic_stats[["Ho"]]
-library(poppr)
-genind2genalex(gind_filter7_loc_cr,
-               filename = "gind_filter7_loc_cr.csv",
-               overwrite = FALSE,
-               quiet = FALSE,
-               pop = NULL,
-               allstrata = TRUE,
-               geo = FALSE,
-               sep = ",",
-               sequence = TRUE)
-gind_filter7_loc_cr
-genind2genalex(gind_filter7_loc_cr,
-               filename = "gind2.csv")
-library(adegenet)
